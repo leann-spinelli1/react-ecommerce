35caeec8d8bd27974e64353b48b8a70f3ab1fa04
Leandro <spinelli.mleandro@gmail.com>
2023-08-23 12:06:09 -0300

    v

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenidos'}/>
+    </div>
+  );
+}
+
+
+
+export default App;
